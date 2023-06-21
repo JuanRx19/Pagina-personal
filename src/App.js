e90bb3a536169e8c778acf33a24cf8f2b96a5ff9
JuanRx19 <63820244+JuanRx19@users.noreply.github.com>
2023-06-20 21:51:49 -0500

    Apartado inicio

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
+import React from 'react';
 import './App.css';
+import Inicio from './components/Inicio';
+import SobreMi from './components/SobreMi';
+import Skills from './components/Skills';
+import Navbar from './components/Navbar';
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Inicio />} />
+          <Route path="/SobreMi" element={<SobreMi />} />
+          <Route path="/Skills" element={<Skills />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import '@fortawesome/fontawesome-svg-core'
+import '@fortawesome/free-brands-svg-icons'
+import '@fortawesome/free-solid-svg-icons'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
